191cfa79b3aaba331d8a07e010a0618cefafe965
nalvarez508 <nickalvarez189@gmail.com>
2023-03-23 14:18:58 -0700

    Remove old files

--- tests/BaseFile.py
+++
@@ -1,9 +0,0 @@
-import sys
-sys.path.append("../../")
-
-import modules
-import numpy as np
-from sklearn import model_selection
-
-mydata = modules.NT("../../data/b100d30.csv")
-x_train, x_test, y_train, y_test = model_selection.train_test_split(mydata.data, mydata.target, test_size=0.4, random_state=508)
